5aa14aa7d8846e62dcd2ce499f8b9686b785bf5b
João Otavio  Mesquita <93503467+Otavio17061992@users.noreply.github.com>
2025-05-18 17:54:19 +0000

    Atualizacao da pages.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ function Home() {
   return (
     <dev>
       <h1>O futuro pertence a Deus, e eu serei a espada do anjo Gabriel</h1>
+      <h2>Aqui Esta o meu Primeiro Projeto que dei Continuidade</h2>
+      <p>Nada que eu não possa realizar</p>
     </dev>
   );
 }
