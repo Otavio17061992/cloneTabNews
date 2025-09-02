b74db1045f8c363a095545943b1ca77efc69684f
Jon.ocx <joaoalmeida48@outlook.com>
2025-09-01 22:14:57 -0300

    Alterado Footer para teste do robo Versel

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,10 @@ function Home()
       <h1>O futuro pertence a Deus, e eu serei a espada do anjo Gabriel</h1>
       <h2>Aqui Esta o meu Primeiro Projeto que dei Continuidade</h2>
       <p>Nada que eu não possa realizar</p>
+
+      <footer>
+        <p>Desenvolvido por João Mesquita</p>
+      </footer>
     </dev>
   );
 }
