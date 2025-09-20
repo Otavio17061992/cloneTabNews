5929847a54db7cc92b8079daec0644c86011ec5e
João Otavio  Mesquita <93503467+Otavio17061992@users.noreply.github.com>
2025-09-20 21:59:59 +0000

    finaliza o endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,30 @@
 import database from "Infra/database.js";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  response.status(200).json({ chave: "Acima da média" });
+  const updatedAt = new Date().toISOString();
+  const databaseName = process.env.POSTGRES_DB;
+  const DatabaseVerisonResult = await database.query("SHOW server_version;");
+  const MaxConnectionDB = await database.query("SHOW max_connections;");
+  const connetionsOpend = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseversionValue = DatabaseVerisonResult.rows[0].server_version;
+  const dataServerMaxConnection = MaxConnectionDB.rows[0].max_connections;
+  const connectionsOpened = connetionsOpend.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        max_connections: parseInt(dataServerMaxConnection),
+        verison: databaseversionValue,
+        opened_connections: connectionsOpened,
+      },
+    },
+  });
 }
 
 export default status;
